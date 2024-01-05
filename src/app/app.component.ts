1cec08bdcdb4e4274f30c4434bc7a2aaad88312b
SSSagare <102542150+Sakshi0305@users.noreply.github.com>
2024-01-05 10:31:42 +0530

    Added Assignment 8 Question 1 Solution

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'A8-Question1';
+  public message :any;
+}
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  public Name :string = "";
+  @Output() public LaserLight = new EventEmitter();
+
+  public Send()
+  {
+    this.LaserLight.emit(this.Name);
+  }
+
+}
